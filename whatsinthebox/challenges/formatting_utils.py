8c4b6f689d4c904bbcd274f67878c3be179d46d2
mayshvets <mayshvets@gmail.com>
2016-08-05 02:45:17 +0300

    added formatting functionality

---
+++ whatsinthebox/challenges/formatting_utils.py
@@ -0,0 +1,27 @@
+class FormattingException(Exception):
+    pass
+
+def format_int(input_string):
+    try:
+        result=int(input_string)
+    except ValueError:
+        raise FormattingException("Couldn't parse as int.")
+
+    return result
+
+def format_float(input_string):
+    try:
+        result = float(input_string)
+    except ValueError:
+        raise FormattingException("Couldn't parse as float.")
+
+    return result
+
+def format_list(input_string):
+    try:
+        result = [float(curr) for curr in input_string.split(" ") if len(curr) > 0]
+    except ValueError:
+        raise FormattingException("Couldn't parse the numbers entered.\n\
+         Please make sure you entered a list on numbers with spaces as separators.")
+
+    return result
---
+++ whatsinthebox/challenges/input_formatter.py
@@ -0,0 +1,4 @@
+class InputFormatter(object):
+    def __init__(self, conversion_function, info_message):
+        self.conversion_function = conversion_function
+        self.info_message = info_message
