b77b726a5136fabe7f439459362167be4993a153
liran <liran.markin@gmail.com>
2016-08-05 06:14:34 +0300

    sorting working

--- whatsinthebox/challenges/binary_challenge.py
+++ whatsinthebox/challenges/binary_challenge.py
@@ -9,9 +9,9 @@ from whatsinthebox.challenges.input_formatter import IOFormatter
 class SortChallenge(BaseChallenge):
     level = 2
     test_cases = [
-        TestCase((0), "0"),
-        TestCase((255), "255"),
-        TestCase((8), "8")]
+        TestCase((0,), "0"),
+        TestCase((255,), "255"),
+        TestCase((8,), "8")]
 
     expected_input = [
         IOFormatter(format_positive_int, "Enter a positive number: ")]
