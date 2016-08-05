9b6402f7f8c524668f4965f3ae73778b8886e3ca
עופר קוטק < >
2016-08-05 03:05:04 +0300

    added example

--- whatsinthebox/challenges/addingchallenge.py
+++ whatsinthebox/challenges/addingchallenge.py
@@ -1,11 +1,21 @@
 from challenges.core.basechallenge import BaseChallenge
 from challenges.core.testcase import TestCase
 
-
 # noinspection PyMethodOverriding
+from challenges.formatting_utils import format_int
+from challenges.input_formatter import InputFormatter
+
+
 class AddingChallenge(BaseChallenge):
     level = 0
-    test_cases = [TestCase((1, 2), "1, 2 ")]
+    test_cases = [
+        TestCase((13, 2), "1, 2 "),
+        TestCase((-3, 2), "1, 2 "),
+        TestCase((0, 4), "1, 2 ")]
+
+    expected_input = [
+        InputFormatter(format_int, "Enter an integer."),
+        InputFormatter(format_int, "Enter another integer.")]
 
     @staticmethod
     def query(first_number, second_number):
