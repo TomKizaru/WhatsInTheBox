68822b072f4f4c4f972b80cbc0db08c51ef17113
עופר קוטק < >
2016-08-05 03:26:31 +0300

    added result formatter

--- whatsinthebox/challenges/addingchallenge.py
+++ whatsinthebox/challenges/addingchallenge.py
@@ -9,13 +9,15 @@ from challenges.input_formatter import InputFormatter
 class AddingChallenge(BaseChallenge):
     level = 0
     test_cases = [
-        TestCase((13, 2), "1, 2 "),
-        TestCase((-3, 2), "1, 2 "),
-        TestCase((0, 4), "1, 2 ")]
+        TestCase((13, 2), "13, 2 "),
+        TestCase((-3, 2), "-3, 2 "),
+        TestCase((0, 4), "0, 4 ")]
 
     expected_input = [
         InputFormatter(format_int, "Enter an integer."),
         InputFormatter(format_int, "Enter another integer.")]
+
+    result_formatter = InputFormatter(format_int, "Please enter an integer.")
 
     @staticmethod
     def query(first_number, second_number):
--- whatsinthebox/challenges/core/basechallenge.py
+++ whatsinthebox/challenges/core/basechallenge.py
@@ -1,10 +1,10 @@
-
 class BaseChallenge(object):
     level = None
     expected_input = None
     test_cases = None
-    
+
+    result_formatter = None
+
     @staticmethod
     def query(*args):
         raise NotImplemented("this is the base class.")
-
